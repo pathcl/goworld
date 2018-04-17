782228c4476cd43ae22502238b18e5e4220fec25
Luis San Martin <luis@sanmartin.io>
2018-04-17 14:16:23 -0300

    bump version

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "0.0.3 %s\n", r.URL.Path[1:])
+	fmt.Fprintf(w, "0.0.2 %s\n", r.URL.Path[1:])
 	log.Println(r.RemoteAddr, r.Method, r.URL)
 }
 
