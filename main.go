92253d5711e162183a08809729085a37952eed44
Luis San Martin <luis@sanmartin.io>
2019-03-27 22:28:28 +0100

    next stop: prometheus

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
+// display homero
+// display clock
+// expose prometheus metrics
+//	> homero
+//	> clock covilha
+//
+// Nice to have: go test
+
 package main
 
 import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "0.0.5%s\n", r.URL.Path[1:])
+	if r.URL.Path[1:] == "covilha" {
+		fmt.Fprintf(w, "%s", time.Now().UTC().Format("15:04:05\n"))
+	} else if r.URL.Path[1:] == "homerosimpson" {
+		fmt.Fprintf(w, "%s", time.Now().Format("15:04:05\n"))
+	} else {
+		fmt.Fprintf(w, "%s\n", r.URL.Path[1:])
+	}
 	log.Println(r.RemoteAddr, r.Method, r.URL)
 }
-
 func main() {
 	http.HandleFunc("/", handler)
 	log.Println("Starting...")
