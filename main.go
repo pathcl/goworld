d4cdf66744ff80c029b76d4f1e98fc7e44949df1
Luis San Martin <l.sanmartin@payvision.com>
2019-03-30 18:01:12 +0100

    8080 port

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func handlerClock(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr: ":8080",
 	}
 	http.HandleFunc("/homerosimpson", handlerHomero)
 	http.HandleFunc("/clock", handlerClock)
